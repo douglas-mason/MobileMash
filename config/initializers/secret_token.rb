5c3379f33f5bad77cc351f11d1a515e3725877bd
Matthew Orres <matthew.orres@gmail.com>
2013-02-03 21:31:37 -0500

    Initial commit of scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileMash::Application.config.secret_token = '3f2f2a77491c1163adbc2386b9e819131c14646ca817baad9fb144484c20d09a1839d6781dd278c0285edb39654ba3eeae5594e06468f636ba1590517406ac31'
