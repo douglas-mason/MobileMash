1d84d68924477a4d6608642ad828a397e1eddcc1
Douglas Mason <dmason@truefitsolutions.com>
2013-02-08 13:55:38 -0500

    removes experimental mongodb code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,5 @@
-include Mongo
-
 class HomeController < ApplicationController
   def index
-    @client = MongoClient.new('localhost', 27017)
-    @db = @client['mobilemash']
-    @coll = @db['sessions']
-    @mash_session = @coll.find_one
+
   end
 end
